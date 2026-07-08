b1b3689015b87b1d56795397fd1cf413ccd0c8bd
Olaroungbe jamiu <olaabiodun157@gmail.com>
2026-07-08 21:05:14 +0100

    Fix eager MongoDB connection causing Netlify

--- proxy.ts
+++ proxy.ts
@@ -1,25 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { getSession } from "./lib/auth/auth";
 
 export default async function proxy(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  const session = await getSession();
+
+  // const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
+  // if(isDashboardPage && !session?.user){
+  //   return NextResponse.redirect(new URL("/sign-in", request.url));
+  // }
 
   const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
   const isSignUpPage = request.nextUrl.pathname.startsWith("/sign-up");
 
-  if ((isSignInPage || isSignUpPage) && sessionCookie) {
+  if ((isSignInPage || isSignUpPage) && session?.user) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  // Uncomment when ready to enforce dashboard protection:
-  // const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
-  // if (isDashboardPage && !sessionCookie) {
-  //   return NextResponse.redirect(new URL("/sign-in", request.url));
-  // }
-
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/dashboard/:path*", "/sign-in", "/sign-up"],
-};
